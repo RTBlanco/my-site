ba91bcd2aa7a7aea0389ec965f499c7cb6b34ec9
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-23 17:27:40 +0000

    added testimonials

--- app/javascript/pages/containers/About.jsx
+++ app/javascript/pages/containers/About.jsx
@@ -1,4 +1,111 @@
+// icons
+import IconDesign from "../../images/icon-design.svg"
+import IconDev from "../../images/icon-dev.svg"
+import IconApp from "../../images/icon-app.svg"
+import IconPhoto from "../../images/icon-photo.svg"
+
+// testimonials
+
+import Avatar from "../../images/avatar-1.png"
+import Avatar2 from "../../images/avatar-2.png"
+import Avatar3 from "../../images/avatar-3.png"
+import Avatar4 from "../../images/avatar-4.png"
+
+
 const About = () => {
+  const serviceItems = [
+    {
+      title: "Web Design",
+      image: IconDesign,
+      text: "The most modern and high-quality design made at a professional level."
+    },
+    {
+      title: "Web Development",
+      image: IconDev,
+      text: "High-quality development of sites at the professional level."
+    },
+    {
+      title: "Mobile Apps",
+      image: IconApp,
+      text: "Professional development of applications for iOS and Android."
+    },
+    {
+      title: "Photography",
+      image: IconPhoto,
+      text: "I make high-quality photos of any category at a professional level."
+    }
+  ];
+
+  const testimonialsItems = [
+  {
+    name: "Daniel Lewis",
+    avatar: Avatar,
+    text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
+  },
+  {
+    name: "Jessica Miller",
+    avatar: Avatar2,
+    text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
+  },
+  {
+    name: "Emily Evans",
+    avatar: Avatar3,
+    text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
+  },
+  {
+    name: "Henry William",
+    avatar: Avatar4,
+    text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
+  }
+];
+
+
+
+  const renderServiceItems = () => {
+    return serviceItems.map((item, index) => {
+      return ( 
+        <li key={index} className="service-item">
+          <div className="service-icon-box">
+            <img src={item.image} alt="design icon" width="40" />
+          </div>
+
+          <div className="service-content-box">
+            <h4 className="h4 service-item-title">{item.title}</h4>
+
+            <p className="service-item-text">
+              {item.text}
+            </p>
+          </div>
+
+        </li>
+      )
+    })
+  }
+
+
+  const renderTestimonials = () => {
+    return testimonialsItems.map((item, index) => {
+      return (
+        <li key={index} className="testimonials-item">
+          <div className="content-card" data-testimonials-item>
+
+            <figure className="testimonials-avatar-box">
+              <img src={item.avatar} alt={item.name} width="60" data-testimonials-avatar />
+            </figure>
+
+            <h4 className="h4 testimonials-item-title" data-testimonials-title>{item.name}</h4>
+
+            <div className="testimonials-text" data-testimonials-text>
+              <p>
+                {item.text}
+              </p>
+            </div>
+          </div>
+        </li>
+      )
+    })
+  }
+
   return (
     <article className="about  active" data-page="about">
 
@@ -32,72 +139,7 @@ const About = () => {
         <h3 className="h3 service-title">What i'm doing</h3>
 
         <ul className="service-list">
-
-          <li className="service-item">
-
-            <div className="service-icon-box">
-              <img src="./assets/images/icon-design.svg
-                " alt="design icon" width="40" />
-            </div>
-
-            <div className="service-content-box">
-              <h4 className="h4 service-item-title">Web design</h4>
-
-              <p className="service-item-text">
-                The most modern and high-quality design made at a professional level.
-              </p>
-            </div>
-
-          </li>
-
-          <li className="service-item">
-
-            <div className="service-icon-box">
-              <img src="./assets/images/icon-dev.svg" alt="Web development icon" width="40" />
-            </div>
-
-            <div className="service-content-box">
-              <h4 className="h4 service-item-title">Web development</h4>
-
-              <p className="service-item-text">
-                High-quality development of sites at the professional level.
-              </p>
-            </div>
-
-          </li>
-
-          <li className="service-item">
-
-            <div className="service-icon-box">
-              <img src="./assets/images/icon-app.svg" alt="mobile app icon" width="40" />
-            </div>
-
-            <div className="service-content-box">
-              <h4 className="h4 service-item-title">Mobile apps</h4>
-
-              <p className="service-item-text">
-                Professional development of applications for iOS and Android.
-              </p>
-            </div>
-
-          </li>
-
-          <li className="service-item">
-
-            <div className="service-icon-box">
-              <img src="./assets/images/icon-photo.svg" alt="camera icon" width="40" />
-            </div>
-
-            <div className="service-content-box">
-              <h4 className="h4 service-item-title">Photography</h4>
-
-              <p className="service-item-text">
-                I make high-quality photos of any category at a professional level.
-              </p>
-            </div>
-
-          </li>
-
+          {renderServiceItems()}
         </ul>
 
       </section>
@@ -232,7 +274,7 @@ const About = () => {
 
             <h4 className="h3 modal-title" data-modal-title>Daniel lewis</h4>
 
-            <time datetime="2021-06-14">14 June, 2021</time>
+            <time dateTime="2021-06-14">14 June, 2021</time>
 
             <div data-modal-text>
               <p>
