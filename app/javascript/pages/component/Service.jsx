9e455570749a423b9dec4f9f7a06bbaeeeb31b80
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-23 17:48:27 +0000

    moved content from about to their own component

---
+++ app/javascript/pages/component/Service.jsx
@@ -0,0 +1,64 @@
+import IconDesign from "../../images/icon-design.svg"
+import IconDev from "../../images/icon-dev.svg"
+import IconApp from "../../images/icon-app.svg"
+import IconPhoto from "../../images/icon-photo.svg"
+
+const Service = () => {
+  const serviceItems = [
+    {
+      title: "Web Design",
+      image: IconDesign,
+      text: "The most modern and high-quality design made at a professional level."
+    },
+    {
+      title: "Web Development",
+      image: IconDev,
+      text: "High-quality development of sites at the professional level."
+    },
+    {
+      title: "Mobile Apps",
+      image: IconApp,
+      text: "Professional development of applications for iOS and Android."
+    },
+    {
+      title: "Photography",
+      image: IconPhoto,
+      text: "I make high-quality photos of any category at a professional level."
+    }
+  ];
+
+  const renderServiceItems = () => {
+    return serviceItems.map((item, index) => {
+      return ( 
+        <li key={index} className="service-item">
+          <div className="service-icon-box">
+            <img src={item.image} alt="design icon" width="40" />
+          </div>
+
+          <div className="service-content-box">
+            <h4 className="h4 service-item-title">{item.title}</h4>
+
+            <p className="service-item-text">
+              {item.text}
+            </p>
+          </div>
+
+        </li>
+      )
+    })
+  }
+
+  return(
+    <section className="service">
+
+      <h3 className="h3 service-title">What i'm doing</h3>
+
+      <ul className="service-list">
+        {renderServiceItems()}
+      </ul>
+
+    </section>
+  )
+}
+
+export default Service;
---
+++ app/javascript/pages/component/Testimonial.jsx
@@ -0,0 +1,65 @@
+
+import Avatar from "../../images/avatar-1.png"
+import Avatar2 from "../../images/avatar-2.png"
+import Avatar3 from "../../images/avatar-3.png"
+import Avatar4 from "../../images/avatar-4.png"
+
+const Testimonial = () => {
+  const testimonialsItems = [
+    {
+      name: "Daniel Lewis",
+      avatar: Avatar,
+      text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
+    },
+    {
+      name: "Jessica Miller",
+      avatar: Avatar2,
+      text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
+    },
+    {
+      name: "Emily Evans",
+      avatar: Avatar3,
+      text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
+    },
+    {
+      name: "Henry William",
+      avatar: Avatar4,
+      text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
+    }
+  ];
+
+  const renderTestimonials = () => {
+    return testimonialsItems.map((item, index) => {
+      return (
+        <li key={index} className="testimonials-item">
+          <div className="content-card" data-testimonials-item>
+
+            <figure className="testimonials-avatar-box">
+              <img src={item.avatar} alt={item.name} width="60" data-testimonials-avatar />
+            </figure>
+
+            <h4 className="h4 testimonials-item-title" data-testimonials-title>{item.name}</h4>
+
+            <div className="testimonials-text" data-testimonials-text>
+              <p>
+                {item.text}
+              </p>
+            </div>
+          </div>
+        </li>
+      )
+    })
+  }
+
+  return(
+    <section className="testimonials">
+      <h3 className="h3 testimonials-title">Testimonials</h3>
+
+      <ul className="testimonials-list has-scrollbar">
+        {renderTestimonials()}
+      </ul>
+    </section>
+  )
+}
+
+export default Testimonial;
