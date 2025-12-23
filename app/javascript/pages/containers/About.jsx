280118451b27f5b4ed1f9b97c2e7c6d421546e25
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-23 17:48:11 +0000

    removed some content

--- app/javascript/pages/containers/About.jsx
+++ app/javascript/pages/containers/About.jsx
@@ -1,111 +1,9 @@
-// icons
-import IconDesign from "../../images/icon-design.svg"
-import IconDev from "../../images/icon-dev.svg"
-import IconApp from "../../images/icon-app.svg"
-import IconPhoto from "../../images/icon-photo.svg"
+import Service from "../component/Service"
+import Testimonial from "../component/Testimonial"
 
-// testimonials
-
-import Avatar from "../../images/avatar-1.png"
-import Avatar2 from "../../images/avatar-2.png"
-import Avatar3 from "../../images/avatar-3.png"
-import Avatar4 from "../../images/avatar-4.png"
 
 
 const About = () => {
-  const serviceItems = [
-    {
-      title: "Web Design",
-      image: IconDesign,
-      text: "The most modern and high-quality design made at a professional level."
-    },
-    {
-      title: "Web Development",
-      image: IconDev,
-      text: "High-quality development of sites at the professional level."
-    },
-    {
-      title: "Mobile Apps",
-      image: IconApp,
-      text: "Professional development of applications for iOS and Android."
-    },
-    {
-      title: "Photography",
-      image: IconPhoto,
-      text: "I make high-quality photos of any category at a professional level."
-    }
-  ];
-
-  const testimonialsItems = [
-    {
-      name: "Daniel Lewis",
-      avatar: Avatar,
-      text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
-    },
-    {
-      name: "Jessica Miller",
-      avatar: Avatar2,
-      text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
-    },
-    {
-      name: "Emily Evans",
-      avatar: Avatar3,
-      text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
-    },
-    {
-      name: "Henry William",
-      avatar: Avatar4,
-      text: "Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia."
-    }
-  ];
-
-
-
-  const renderServiceItems = () => {
-    return serviceItems.map((item, index) => {
-      return ( 
-        <li key={index} className="service-item">
-          <div className="service-icon-box">
-            <img src={item.image} alt="design icon" width="40" />
-          </div>
-
-          <div className="service-content-box">
-            <h4 className="h4 service-item-title">{item.title}</h4>
-
-            <p className="service-item-text">
-              {item.text}
-            </p>
-          </div>
-
-        </li>
-      )
-    })
-  }
-
-
-  const renderTestimonials = () => {
-    return testimonialsItems.map((item, index) => {
-      return (
-        <li key={index} className="testimonials-item">
-          <div className="content-card" data-testimonials-item>
-
-            <figure className="testimonials-avatar-box">
-              <img src={item.avatar} alt={item.name} width="60" data-testimonials-avatar />
-            </figure>
-
-            <h4 className="h4 testimonials-item-title" data-testimonials-title>{item.name}</h4>
-
-            <div className="testimonials-text" data-testimonials-text>
-              <p>
-                {item.text}
-              </p>
-            </div>
-          </div>
-        </li>
-      )
-    })
-  }
-
   return (
     <article className="about  active" data-page="about">
 
@@ -130,39 +28,17 @@ const About = () => {
       </section>
 
 
-      {/* <!--
-      - service
-        --> */}
+      {/* <!--- service --> */}
 
-      <section className="service">
-
-        <h3 className="h3 service-title">What i'm doing</h3>
-
-        <ul className="service-list">
-          {renderServiceItems()}
-        </ul>
-
-      </section>
+      <Service />
 
 
-      {/* <!--
-      - testimonials
-        --> */}
+      {/* <!--- testimonials--> */}
 
-      <section className="testimonials">
-
-        <h3 className="h3 testimonials-title">Testimonials</h3>
-
-        <ul className="testimonials-list has-scrollbar">
-          {renderTestimonials()}
-        </ul>
-
-      </section>
+      <Testimonial />
 
 
-      {/* <!--
-      - testimonials modal
-        --> */}
+      {/* <!--- testimonials modal--> */}
 
       <div className="modal-container" data-modal-container>
 
@@ -204,10 +80,7 @@ const About = () => {
 
       </div>
 
-{/* 
-      <!--
-      - clients
-        --> */}
+      {/* <!--- clients --> */}
 
       <section className="clients">
 
