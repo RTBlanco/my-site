95a7b4fdd1ca75b41d28e4228114011ef9c1b0b2
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-28 22:47:54 +0000

    added admin panel

---
+++ app/javascript/pages/containers/Admin.jsx
@@ -0,0 +1,21 @@
+export default function Admin() {
+  return (
+    <article className="admin active" data-page="admin">
+
+      <header>
+        <h2 className="h2 article-title">Admin</h2>
+      </header>
+
+      <section className="blog-posts">
+
+        <ul className="blog-posts-list">
+
+          {/* {renderBlogs()} */}
+
+        </ul>
+
+      </section>
+
+    </article>
+  )
+}
