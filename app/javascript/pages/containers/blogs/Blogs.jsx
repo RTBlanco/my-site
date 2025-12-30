72523278d01cd727db5faa8f0c0cf455ad0bc5bf
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-30 19:41:01 +0000

    updated images

--- app/javascript/pages/containers/blogs/Blogs.jsx
+++ app/javascript/pages/containers/blogs/Blogs.jsx
@@ -1,9 +1,9 @@
-import blog1 from "../../images/blog-1.jpg"
-import blog2 from "../../images/blog-2.jpg"
-import blog3 from "../../images/blog-3.jpg"
-import blog4 from "../../images/blog-4.jpg"
-import blog5 from "../../images/blog-5.jpg"
-import blog6 from "../../images/blog-6.jpg"
+import blog1 from "../../../images/blog-1.jpg"
+import blog2 from "../../../images/blog-2.jpg"
+import blog3 from "../../../images/blog-3.jpg"
+import blog4 from "../../../images/blog-4.jpg"
+import blog5 from "../../../images/blog-5.jpg"
+import blog6 from "../../../images/blog-6.jpg"
 
 const Blogs = () => {
   const blogPosts = [
