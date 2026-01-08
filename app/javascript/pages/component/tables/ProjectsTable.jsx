d7eef2d76ca7cd349b3ce73c5db91a46806392c7
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-08 15:42:06 +0000

    added correct html to projects table

---
+++ app/javascript/pages/component/tables/ProjectsTable.jsx
@@ -0,0 +1,34 @@
+const ProjectsTable = ({children}) => {
+
+  return (
+    <div className="table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            <th>Project</th>
+            <th>Category</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {children.map(child => (
+            <tr key={child.id}>
+              <td>
+                <div className="project-cell">
+                  <div className="project-image" style={{ background: "linear-gradient(135deg, #667eea 0%, #764ba2 100%);" }}></div>
+                  <div className="project-info">
+                    <h3>{child.title}</h3>
+                  </div>
+                </div>
+              </td>
+              <td><span className="tag webdev">{child.category}</span></td>
+              <td>{child.year}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default ProjectsTable;
