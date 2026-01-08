d08a50ab1b886ab1440cf910b663fd81fa5ae6ad
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-08 15:06:29 +0000

    created Login

--- app/javascript/pages/containers/admin/AdminLogin.jsx
+++ app/javascript/pages/containers/admin/AdminLogin.jsx
@@ -1,6 +1,31 @@
 const AdminLogin = () => {
   return(
-    <>AdminLogin</>
+    <article className="admin-login active" data-page="admin-login">
+
+      <header>
+        <h2 className="h2 article-title">Login</h2>
+      </header>
+
+      <section className="contact-form">
+
+        <form action="#" className="form" data-form>
+
+          <div className="input-wrapper">
+            <input type="text" name="email" className="form-input" placeholder="Email" required data-form-input />
+
+            <input type="email" name="password" className="form-input" placeholder="Password" required data-form-input />
+          </div>
+
+          <button className="form-btn" type="submit" disabled data-form-btn>
+            <ion-icon name="paper-plane"></ion-icon>
+            <span>Login</span>
+          </button>
+
+        </form>
+
+      </section>
+
+    </article>
   )
 }
 
