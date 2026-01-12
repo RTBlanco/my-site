bae4ca4c9180def7968369650dcd730a00749c14
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-12 14:47:05 +0000

    made serialization method

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,10 @@
 class Blog < ApplicationRecord
   has_one_attached :image
+
+  def serialized
+    serializable_hash(except: [ :created_at, :updated_at ]).merge(
+      "dateISO" => created_at.to_date.iso8601,
+      "dateLabel" => created_at.strftime("%b %d, %Y")
+    )
+  end
 end
