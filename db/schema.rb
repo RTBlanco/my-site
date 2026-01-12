d975b28dccb2ceb58dfacbcd3f839ff4688c03f8
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-12 11:18:57 +0000

    added category to Blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_07_233815) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_12_111732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_07_233815) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "projects", force: :cascade do |t|
