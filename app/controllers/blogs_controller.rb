0444300f62f4788d6e49a84268fe65b6c5aadf16
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-12 14:30:43 +0000

    serialized blogs in controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,8 +8,19 @@ class BlogsController < ApplicationController
   end
 
   def index
+    blogs = Blog.all.map do |blog|
+      {
+        id: blog.id,
+        title: blog.title,
+        category: blog.category,
+        dateISO: blog.created_at.strftime("%F"),
+        dateLabel: blog.created_at.strftime("%b %d, %Y"),
+        content: blog.content,
+        image: blog.image
+      }
+    end
 
     # render all the blogs
-    render inertia: "containers/blogs/Blogs"
+    render inertia: "containers/blogs/Blogs", props: { blogs: }
   end
 end
