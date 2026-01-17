a5597fb5b0a2683a3d34fa6b5412ec99c5778cc9
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-17 02:46:00 +0000

    created serializer

---
+++ app/serializers/blog_serializer.rb
@@ -0,0 +1,13 @@
+class BlogSerializer
+  def initialize(blog)
+    @blog = blog
+  end
+
+  def as_json
+    @blog.serializable_hash(except: [ :created_at, :updated_at, :image ]).merge(
+      "dateISO" => @blog.created_at.to_date.iso8601,
+      "dateLabel" => @blog.created_at.strftime("%b %d, %Y"),
+      "image" => url_for(@blog.image)
+    )
+  end
+end
