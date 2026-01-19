2c0182342e61fcf8b24fd88fb38a314bb6062430
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-19 14:07:21 +0000

    removed fragment

--- app/javascript/pages/component/tables/Table.jsx
+++ app/javascript/pages/component/tables/Table.jsx
@@ -1,35 +1,33 @@
 const Table = ({children, name}) => {
 
   return (
-    <>
-      <div className="table-wrapper">
-        <table>
-          <thead>
-            <tr>
-              <th>{name}</th>
-              <th>Category</th>
-              <th>Year</th>
-            </tr>
-          </thead>
-          <tbody>
-            {children.map(child => (
-              <tr key={child.id}>
-                <td>
-                  <div className="project-cell">
-                    <div className="project-image" style={{ background: "linear-gradient(135deg, #667eea 0%, #764ba2 100%);" }}></div>
-                    <div className="project-info">
-                      <h3>{child.title}</h3>
-                    </div>
+    <div className="table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            <th>{name}</th>
+            <th>Category</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {children.map(child => (
+            <tr key={child.id}>
+              <td>
+                <div className="project-cell">
+                  <div className="project-image" style={{ background: "linear-gradient(135deg, #667eea 0%, #764ba2 100%);" }}></div>
+                  <div className="project-info">
+                    <h3>{child.title}</h3>
                   </div>
-                </td>
-                <td><span className="tag webdev">{child.category}</span></td>
-                <td>{child.dateLabel.split(', ')[1]}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+                </div>
+              </td>
+              <td><span className="tag webdev">{child.category}</span></td>
+              <td>{child.dateLabel.split(', ')[1]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
