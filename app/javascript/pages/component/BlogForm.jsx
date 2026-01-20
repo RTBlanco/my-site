86f8fc81b828ccb8e8e297d09c5fd138103b8576
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-20 14:08:40 +0000

    toggled modal on submit

--- app/javascript/pages/component/BlogForm.jsx
+++ app/javascript/pages/component/BlogForm.jsx
@@ -1,18 +1,19 @@
 import { Form } from "@inertiajs/react";
 
-const BlogForm = ({blog}) => {
+const BlogForm = ({blog, toggleModal}) => {
 
   const newBlog = () => {
     return Object.keys(blog).length === 0
   }
 
+  console.log(newBlog())
 
   return (
     <section className="blog-form">
 
-      <h3 className="h3 form-title">{newBlog ? "Create Blog" : `Edit ${blog.name}`}</h3>
+      <h3 className="h3 form-title">{newBlog() ? "Create Blog" : `Edit ${blog.title}`}</h3>
 
-      <Form action={newBlog ? "/Blogs" : `/Blogs/${blog.id}`} method={newBlog ? "post" : "patch"} className="Form" data-form>
+      <Form action={newBlog() ? "/Blogs" : `/Blogs/${blog.id}`} method={newBlog() ? "post" : "patch"} className="Form" data-form>
 
         <div className="input-wrapper">
           <input type="file" name="image" className="form-input" placeholder="image" accept="image/*" data-form-input />
@@ -25,7 +26,7 @@ const BlogForm = ({blog}) => {
 
         <textarea defaultValue={blog.content} name="content" className="form-input" placeholder="Content" required data-form-input></textarea>
 
-        <button className="form-btn" type="submit" data-form-btn>
+        <button onClick={toggleModal} className="form-btn" type="submit" data-form-btn>
           <ion-icon name="enter-outline"></ion-icon>
           <span>Save</span>
         </button>
