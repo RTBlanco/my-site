9593a3c098743074accb1d609f8c020a503dadcd
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-21 15:11:12 +0000

    update form to be able to edit and or delete blog

--- app/javascript/pages/component/BlogForm.jsx
+++ app/javascript/pages/component/BlogForm.jsx
@@ -1,35 +1,58 @@
-import { Form } from "@inertiajs/react";
+import { Form, useForm } from "@inertiajs/react";
 
 const BlogForm = ({blog, toggleModal}) => {
 
-  const newBlog = () => {
-    return Object.keys(blog).length === 0
+  const {delete: destroy } = useForm({})
+  const newBlog =  Object.keys(blog).length === 0
+
+  const handleDelete = (e, id) => {
+    e.preventDefault()
+    destroy(`/Blogs/${id}`,{
+      preserveScroll: true
+    })
+    toggleModal()
   }
-
-  console.log(newBlog())
-
+  console.log(newBlog)
   return (
     <section className="blog-form">
 
-      <h3 className="h3 form-title">{newBlog() ? "Create Blog" : `Edit ${blog.title}`}</h3>
+      <h3 className="h3 form-title">{newBlog ? "Create Blog" : `Edit ${blog.title}`}</h3>
 
-      <Form action={newBlog() ? "/Blogs" : `/Blogs/${blog.id}`} method={newBlog() ? "post" : "patch"} className="Form" data-form>
+      <Form 
+        action={newBlog ? "/Blogs" : `/Blogs/${blog.id}`} 
+        method={newBlog ? "post" : "put"} 
+        className="Form" 
+        data-form
+        options={{
+          preserveState: false,
+          preserveScroll: true
+        }}
+        >
 
         <div className="input-wrapper">
           <input type="file" name="image" className="form-input" placeholder="image" accept="image/*" data-form-input />
         </div>
 
         <div className="input-wrapper">
-          <input defaultValue={blog.title} type="text" name="title" className="form-input" placeholder="Blog Title" required data-form-input />
+          <input defaultValue={blog.title} type="text" name="title" className="form-input" placeholder="Blog Title"  data-form-input />
           <input defaultValue={blog.category} type="text" name="category" className="form-input" placeholder="Blog Category" data-form-input />
         </div>
 
-        <textarea defaultValue={blog.content} name="content" className="form-input" placeholder="Content" required data-form-input></textarea>
+        <textarea defaultValue={blog.content} name="content" className="form-input" placeholder="Content"  data-form-input></textarea>
 
-        <button onClick={toggleModal} className="form-btn" type="submit" data-form-btn>
-          <ion-icon name="enter-outline"></ion-icon>
-          <span>Save</span>
-        </button>
+        <div className="form-btns">
+          {newBlog ||
+            <button onClick={(e) => handleDelete(e, blog.id)} className="form-btn" type="submit" data-form-btn>
+              <ion-icon name="trash-bin-outline"></ion-icon>
+              <span>Delete</span>
+            </button>
+          }
+
+          <button onClick={toggleModal} className="form-btn" type="submit" data-form-btn>
+            <ion-icon name="enter-outline"></ion-icon>
+            <span>Save</span>
+          </button>
+        </div>
 
       </Form>
 
