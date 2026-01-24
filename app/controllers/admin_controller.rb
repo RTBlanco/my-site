af00a9a4118db9e7281c44e40852886b2d78bd7f
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-24 19:26:24 +0000

    added login method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,5 @@
 class AdminController < ApplicationController
-  # before_action :authenticate_admin!, only: [:dashboard]
-  before_action :authenticate_admin, only: [:dashboard]
+  before_action :authenticate_admin, only: [ :dashboard ]
 
   def dashboard
     blogs = Blog.all.map do |blog|
@@ -10,7 +9,20 @@ class AdminController < ApplicationController
     render inertia: "containers/admin/Admin", props: { blogs: }
   end
 
+  def sign_admin_in
+    # binding.break
+    @admin = Admin.find_by(email: params[:email])
+
+    if @admin && @admin.valid_password?(params[:password])
+      sign_in(@admin)
+      redirect_to dashboard_path
+    else
+      redirect_to login_path, inertia: { errors: "Incorrect Login" }
+    end
+  end
+
+
   def login
-    binding.break
+    render inertia: "containers/admin/AdminLogin"
   end
 end
