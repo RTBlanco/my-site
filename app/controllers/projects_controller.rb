618d9580f7ad674ade1413d0b16b13c8b36a19f5
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-28 23:48:12 +0000

    added todos

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,11 @@
 class ProjectsController < ApplicationController
+
+  def create
+    # adds link that will gather info on projects
+    # no need for form since it fetches data
+  end
+
+  def destroy
+    # delete projects
+  end
 end
