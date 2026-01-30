4372a234ad2a2b09eaeab4cd2474e6e2f1d5b949
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-30 14:43:53 +0000

    added to json and chanched method name

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -3,11 +3,15 @@ class BlogSerializer
     @blog = blog
   end
 
-  def as_json
+  def as_hash
     @blog.serializable_hash(except: [ :created_at, :updated_at, :image ]).merge(
       "dateISO" => @blog.created_at.to_date.iso8601,
       "dateLabel" => @blog.created_at.strftime("%b %d, %Y"),
       "image" => @blog.image_url
     )
+  end
+
+  def to_json
+    as_hash.to_json
   end
 end
