5d2da588a18e3e7084b8e0fa077543a5737a44a0
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-30 19:10:35 +0000

    displayed correct projects from db

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,15 +8,14 @@ class ProjectsController < ApplicationController
         repo_name = link.match(%r{https://github\.com/(.+?)\/?$})[1]
         repo = Octokit.repo(repo_name)
 
-        Project.create(title: repo.name, description: repo.description, link: repo.html_url)
+        Project.create(title: repo.name, description: repo.description, link: repo.html_url, category: "coding")
       end
-    # asdad
     redirect_to dashboard_path
   end
 
   def index
-    projects = Blog.all.map do |blog|
-      ProjectSerializer.new(blog).as_hash
+    projects = Project.all.map do |project|
+      ProjectSerializer.new(project).as_hash
     end
     render inertia: "containers/Portfolio", props: { projects: }
   end
