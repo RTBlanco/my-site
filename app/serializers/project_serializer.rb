14f0139871160b7cbf72057dfb9424e8f521a8be
RTBlanco <ronnytoribio1@hotmail.com>
2026-01-30 19:10:20 +0000

    addded image

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -4,9 +4,10 @@ class ProjectSerializer
   end
 
   def as_hash
-    @project.serializable_hash(except: [ :created_at, :updated_at ]).merge(
+    @project.serializable_hash(except: [ :created_at, :updated_at, :image_url ]).merge(
       "dateISO" => @project.created_at.to_date.iso8601,
       "dateLabel" => @project.created_at.strftime("%b %d, %Y"),
+      "image_url" => "https://picsum.photos/200"
     )
   end
 
