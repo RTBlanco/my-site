9a8c09bf3a01aec2a3bd13345b5cd54f67ce016a
RTBlanco <ronnytoribio1@hotmail.com>
2026-02-07 04:18:02 +0000

    updated mailer defaults

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "rtblanco66@gmail.com"
+  default to: "ronnytoribio1@hotmail.com"
   layout "mailer"
 end
