adfb9c2d39c943b5865922a58af934cfe677860a
RTBlanco <ronnytoribio1@hotmail.com>
2026-02-07 04:20:36 +0000

    contact mailer

--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,3 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
+  def contact_email
+    ContactMailer.contact_email("test@email.com", "hi this is just a test", "Mr.tester")
+  end
 end
