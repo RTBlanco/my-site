f2868d96c33dca1366e260440dd7cd66280990ed
RTBlanco <ronnytoribio1@hotmail.com>
2026-02-07 04:18:23 +0000

    created contact maler

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,10 @@
 class ContactMailer < ApplicationMailer
+  # prepend_view_path "views/mailers/contact_mailers/"
+
+  def contact_email(email, message, full_name = "No Name given")
+    @email = email
+    @message = message
+    @full_name = full_name
+    mail(subject: "Contact from Website")
+  end
 end
