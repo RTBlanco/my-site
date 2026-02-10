b2ed510e7341de749a009d9da88832cd02618180
RTBlanco <ronnytoribio1@hotmail.com>
2026-02-10 14:06:57 +0000

    instead of saving ur just used default on front end

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,23 +3,8 @@ class Blog < ApplicationRecord
 
   validates_presence_of :content, :title, :category
 
-  after_commit :add_default_image, on: [:create, :update]
-
   def image_url
     return nil unless image.attached?
     Rails.application.routes.url_helpers.rails_blob_path(image, only_path: true)
-  end
-
-
-  private
-
-  def add_default_image
-    unless image.attached?
-      self.image.attach(
-        io: File.open(Rails.root.join("app", "assets", "images", "default.png")),
-        filename: "default.png",
-        content_type: "image/png"
-      )
-    end
   end
 end
