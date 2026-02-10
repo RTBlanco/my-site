f600c20e8f6ff52aae191d98ffabe017ac14fb88
RTBlanco <ronnytoribio1@hotmail.com>
2026-02-10 14:06:25 +0000

    removed test blogs

--- app/javascript/pages/containers/blogs/Blogs.jsx
+++ app/javascript/pages/containers/blogs/Blogs.jsx
@@ -1,89 +1,12 @@
-import blog1 from "../../../images/blog-1.jpg"
-import blog2 from "../../../images/blog-2.jpg"
-import blog3 from "../../../images/blog-3.jpg"
-import blog4 from "../../../images/blog-4.jpg"
-import blog5 from "../../../images/blog-5.jpg"
-import blog6 from "../../../images/blog-6.jpg"
 
+import defaultImage from "../../../../assets/images/default.png"
 import { Link } from "@inertiajs/react"
 import { usePage } from "@inertiajs/react"
 
 const Blogs = () => {
 
   const { blogs } = usePage().props
-  const blogPosts = [
-    {
-      id: 1,
-      title: "Design conferences in 2022",
-      category: "Design",
-      dateISO: "2022-02-23",
-      dateLabel: "Feb 23, 2022",
-      content: "Veritatis et quasi architecto beatae vitae dicta sunt, explicabo.",
-      image: blog1,
-      alt: "Design conferences in 2022",
-      link: "/Blogs/1",
-    },
-    {
-      id: 2,
-      title: "Best fonts every designer",
-      category: "Design",
-      dateISO: "2022-02-23",
-      dateLabel: "Feb 23, 2022",
-      content: "Sed ut perspiciatis, nam libero tempore, cum soluta nobis est eligendi.",
-      image: blog2,
-      alt: "Best fonts every designer",
-      link: "#",
-    },
-    {
-      id: 3,
-      title: "Design digest #80",
-      category: "Design",
-      dateISO: "2022-02-23",
-      dateLabel: "Feb 23, 2022",
-      content:
-        "Excepteur sint occaecat cupidatat non proident, quis nostrum exercitationem ullam corporis suscipit.",
-      image: blog3,
-      alt: "Design digest #80",
-      link: "#",
-    },
-    {
-      id: 4,
-      title: "UI interactions of the week",
-      category: "Design",
-      dateISO: "2022-02-23",
-      dateLabel: "Feb 23, 2022",
-      content:
-        "Enim ad minim veniam, consectetur adipiscing elit, quis nostrud exercitation ullamco laboris nisi.",
-      image: blog4,
-      alt: "UI interactions of the week",
-      link: "#",
-    },
-    {
-      id: 5,
-      title: "The forgotten art of spacing",
-      category: "Design",
-      dateISO: "2022-02-23",
-      dateLabel: "Feb 23, 2022",
-      content:
-        "Maxime placeat, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: blog5,
-      alt: "The forgotten art of spacing",
-      link: "#",
-    },
-    {
-      id: 6,
-      title: "Design digest #79",
-      category: "Design",
-      dateISO: "2022-02-23",
-      dateLabel: "Feb 23, 2022",
-      content:
-        "Optio cumque nihil impedit quo minus quod maxime placeat, velit esse cillum.",
-      image: blog6,
-      alt: "Design digest #79",
-      link: "#",
-    },
-  ];
-
+  
   const renderBlogs = () => {
 
     return blogs.map(blog => (
@@ -91,7 +14,7 @@ const Blogs = () => {
         <Link href={`/Blogs/${blog.id}`}>
 
           <figure className="blog-banner-box">
-            <img src={blog.image} alt={blog.alt} loading="lazy" />
+            <img src={blog.image || defaultImage} alt={blog.alt} loading="lazy" />
           </figure>
 
           <div className="blog-content">
