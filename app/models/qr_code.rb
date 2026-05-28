36b4ff947563a1d3575e3a07e4ef1e5340724314
RTBlanco <ronnytoribio1@hotmail.com>
2026-05-28 18:06:30 +0000

    changed the size of the code

--- app/models/qr_code.rb
+++ app/models/qr_code.rb
@@ -28,7 +28,7 @@ class QrCode < ApplicationRecord
       module_px_size: 6,
       resize_exactly_to: false,
       resize_gte_to: false,
-      size: 120
+      size: 500
     )
 
     self.code.attach(
